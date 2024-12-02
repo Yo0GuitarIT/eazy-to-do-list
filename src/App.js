6a0600cf5589bd98be48253fd9386f2620a6911e
Yu Ling Chen <yo0.guitar.it@gmail.com>
2024-12-03 01:52:35 +0800

    Refactor: input handler refactor

--- src/App.js
+++ src/App.js
@@ -29,37 +29,47 @@ function App() {
     setTasks(newTasks);
   };
 
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>To Do List</h1>
-        <main>
-          <input
-            type="text"
-            value={inputValue}
-            placeholder="Add a new task"
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-          <button onClick={handleAddTodoTask}>Add</button>
-          <ul>
-            {tasks.map((task, index) => (
-              <li key={index}>
-                <span
-                  style={{
-                    textDecoration: task.completed ? "line-through" : "none",
-                    marginRight: "10px",
-                  }}
-                >
-                  {task.text}
-                </span>
-                <button onClick={handleToggleComplete(index)}>
-                  {task.completed ? "Undo" : "Complete"}
-                </button>
-                <button onClick={handleDeleteOnClick(index)}>Delete</button>
-              </li>
-            ))}
-          </ul>
+        <main style={{ display: "flex" }}>
+          <section>
+            <h2>Add a new task</h2>
+            <input
+              type="text"
+              value={inputValue}
+              placeholder="Write something to do..."
+              onChange={handleInputChange}
+            />
+            <button onClick={handleAddTodoTask}>Add</button>
+          </section>
+          <section>
+            <h2>Tasks</h2>
+            <ul>
+              {tasks.map((task, index) => (
+                <li key={index}>
+                  <span
+                    style={{
+                      textDecoration: task.completed ? "line-through" : "none",
+                      marginRight: "10px",
+                    }}
+                  >
+                    {task.text}
+                  </span>
+                  <button onClick={handleToggleComplete(index)}>
+                    {task.completed ? "Undo" : "Complete"}
+                  </button>
+                  <button onClick={handleDeleteOnClick(index)}>Delete</button>
+                </li>
+              ))}
+            </ul>
+          </section>
         </main>
       </header>
     </div>
