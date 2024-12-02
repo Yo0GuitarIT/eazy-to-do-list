39b9c527062b06075c60e8da96344a464e40f181
Yu Ling Chen <yo0.guitar.it@gmail.com>
2024-12-03 01:17:46 +0800

    Feat: Add Delete Function

--- src/App.js
+++ src/App.js
@@ -6,31 +6,38 @@ function App() {
   const [inputValue, setInputValue] = useState("");
   const [tasks, setTasks] = useState([]);
 
-
   const handleAddTodoTask = () => {
     if (inputValue.trim() === "") {
       return;
     }
     setTasks([...tasks, inputValue]);
     setInputValue("");
-  }
-  
+  };
+
+  const handleDeleteOnClick = (index) => () => {
+    const newTasks = tasks.filter((_task, taskIndex) => taskIndex !== index);
+    setTasks(newTasks);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>To Do List</h1>
-        <main> 
+        <main>
           <input
             type="text"
             value={inputValue}
             placeholder="Add a new task"
             onChange={(e) => setInputValue(e.target.value)}
-          /> 
+          />
           <button onClick={handleAddTodoTask}>Add</button>
           <ul>
             {tasks.map((task, index) => (
-              <li key={index}>{task}</li>
+              <>
+                <li key={index}>{task}</li>
+                <button onClick={handleDeleteOnClick(index)}>Delete</button>
+              </>
             ))}
           </ul>
         </main>
