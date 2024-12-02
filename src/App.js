8871f15535522d6d0082c77d293e76a21a513263
Yu Ling Chen <yo0.guitar.it@gmail.com>
2024-12-03 01:11:59 +0800

    Feat: Add task show list

--- src/App.js
+++ src/App.js
@@ -4,20 +4,35 @@ import "./App.css";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+
+  const handleAddTodoTask = () => {
+    if (inputValue.trim() === "") {
+      return;
+    }
+    setTasks([...tasks, inputValue]);
+    setInputValue("");
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>To Do List</h1>
-        <main>
-          
+        <main> 
           <input
             type="text"
             value={inputValue}
             placeholder="Add a new task"
             onChange={(e) => setInputValue(e.target.value)}
           /> 
-          <button>Add</button>
+          <button onClick={handleAddTodoTask}>Add</button>
+          <ul>
+            {tasks.map((task, index) => (
+              <li key={index}>{task}</li>
+            ))}
+          </ul>
         </main>
       </header>
     </div>
